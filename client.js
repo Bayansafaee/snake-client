e74bb285b1ad3069f91d890128ccc4f3c8145fce
Bayan Safaee <bayan.safaee@gmail.com>
2022-07-06 19:23:05 -0700

    created two functions to handle user input

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243", // IP address here,
     port: 50541 // PORT number here,
@@ -11,16 +11,12 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    conn.write("Name: Dom");
-  });
-
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("Successfully connected to game server")
+    console.log("Successfully connected to game server");
+    conn.write("Name: Dom"); 
   });
 
   conn.on("data", (data) => {
-    console.log(data)
+    console.log(data);
   });
 
   return conn;
@@ -28,6 +24,4 @@ const connect = function () {
 
 console.log("Connecting ...");
 
-
 module.exports = connect;
-
--- play.js
+++ play.js
@@ -1,5 +1,20 @@
-const net = require("net");
-
-const connect = require("./client")
-
+const connect = require("./client");
 connect();
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
